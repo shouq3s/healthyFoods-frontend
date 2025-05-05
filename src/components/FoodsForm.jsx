f09592804620afaa59a3ccfa30cd442370583f3f
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-05-05 20:57:00 +0300

    add bulma css to foods form

--- src/components/FoodsForm.jsx
+++ src/components/FoodsForm.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 function FoodsForm(props) {
   return (
-    <div className="custom-list">
-      <h1 >{props.nameverb} your Foods</h1>
-      <form onSubmit={props.handleSubmit}>                
-                <div>  
-                    <label htmlFor='foodName'>Foods Name:</label>
+    <div className="container">
+      <div className="box">
+      <h1 className="title is-4">{props.nameverb} your Foods</h1>
+      <form onSubmit={props.handleSubmit}> 
+                     
+                <div className="field">  
+                    <label className="label" htmlFor='foodName'>Foods Name:</label>
                     <input
+                        className="input"
                         id='foodName'
                         name='foodName'
                         required
@@ -17,9 +20,10 @@ function FoodsForm(props) {
                         />
                         
                         </div>
-                     <div>  
-                    <label htmlFor='calories'>Food calories:</label>
+                     <div className="field">  
+                    <label className="label" htmlFor='calories'>Food calories:</label>
                         <input
+                            className="input"
                             id='calories'
                             name='calories'
                             required
@@ -28,9 +32,10 @@ function FoodsForm(props) {
 
                             />
                     </div>  
-                    <div>  
-                    <label htmlFor='Protien'>Protein:</label>
+                    <div className="field">  
+                    <label className="label" htmlFor='Protien'>Protein:</label>
                     <input
+                        className="input"
                         id='Protien'
                         name='Protien'
                         required
@@ -39,9 +44,10 @@ function FoodsForm(props) {
 
                         />
                     </div>   
-                    <div> 
-                    <label htmlFor='Fiber'>Fiber:</label>
+                    <div className="field"> 
+                    <label className="label" htmlFor='Fiber'>Fiber:</label>
                     <input
+                        className="input"
                         id='Fiber'
                         name='Fiber'
                         required
@@ -49,27 +55,30 @@ function FoodsForm(props) {
                         onChange={event => props.setFiber(event.target.value)}
 
                         /> 
-                     <div>   
-                    <label htmlFor='Ingredients'>Food Ingredients:</label>
+                     <div className="field">   
+                    <label className="label" htmlFor='Ingredients'>Food Ingredients:</label>
                     <input
+                        className="textarea"
                         id='Ingredients'
                         name='Ingredients'
                         required
                         value={props.Ingredients}
                         onChange={event => props.setIngredients(event.target.value)}
                         /> 
-                      <div>
-                      <label htmlFor='imgUpload'>Image</label>
+                      <div className="field">
+                      <label className="label" htmlFor='imgUpload'>Image</label>
                     <input 
+                        
                         type='file'
                         accept='image/*'
                          onChange={event => props.setImageFile(event.target.files[0])}
                       />
                         </div> 
                 </div>     
-                <button type='submit'>Submit</button>
+                <button className="button is-primary" type='submit'>Submit</button>
                 </div>
-                </form>
+                
+                </form></div>
     </div>
   )
 }
