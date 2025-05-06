7af9a73115fee59c2718fe949403e43ddd8e7304
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-05-06 12:59:39 +0300

    add css to navbar page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,17 @@ function NavBar() {
   return (
     <>
 
-     <nav >
+     <nav  className="tabs is-centered is-boxed">
+     <div className="container">
      <ul>
-      <Link to='/'>Home </Link>|
+      
      <Link to='/healthyfoods'>HealthyFoods </Link>|
      <Link to='/healthydrinks'>healthy Drink   </Link>|
      <Link to='/healthyfoods/new'> Add HealthyFoods</Link> |
      <Link to='/healthydrinks/new'>new healthy Drink</Link>|
      <Link to='/logout'>Logout </Link>|
      </ul> 
+     </div>
      </nav>
      
      </>
