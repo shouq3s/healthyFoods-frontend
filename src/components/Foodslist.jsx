cfe7a21fbdf0bc0d199abf97310777b277655ec1
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-05-06 22:11:35 +0300

    adding css to foods list page

--- src/components/Foodslist.jsx
+++ src/components/Foodslist.jsx
@@ -15,33 +15,41 @@ import { authorizedRequest } from '../lib/api'
     },[])
   
   return (
-    <div  >
-      <h1 className="title">all Foods :</h1>
-      <ul className="ul-list" >
-
+    <div className="container" >
+      <h1 className="title is-1 has-text-centered" >all Foods :</h1>
+      <div className="columns is-multiline ">
       {foods.map(food =>{
         return(
          
-          <div className="custom-list" key={food.id}>
+          <div className="column is-one-third" key={food.id}>
+            <div className="box has-border" >
              <Link to={`/healthyfoods/${food.id}`}> 
              {
                 food.image_url
                 ?
+                <figure className="image is-5by4">
                 <img src={food.image_url} 
                 className='customImage'
                 />
+                </figure>
                 :
                 null
             }
-              <p >{food.foodName} with {food.calories}(cals)</p>
-              
+            <div className="content">
+              <h3 className="title is-4">{food.foodName}</h3>
+              <p className="title is-6">{food.calories} calories</p>
+                    <div className="tags">
+                      <span className="tag is-info is-light">Protein: {food.Protien}g</span>
+                      <span className="tag is-info is-light">Fiber: {food.Fiber}g</span>
+             </div> 
+             </div> 
               </Link>
           </div>
-         
+         </div>
         )
       })} 
-      </ul>
-    </div>
+      </div>
+      </div>
   )
 }
 
