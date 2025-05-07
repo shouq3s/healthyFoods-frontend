67b09fc577841fdedf9daae9799bc371b8cfb089
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-05-07 11:00:27 +0300

    adding bulma css to navbar page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,17 @@ import { Link } from 'react-router'
 function NavBar() {
   return (
     <>
-     <nav  className="tabs is-centered is-boxed">
+     <nav  className="navbar is-dark">
      <div className="container">
-     <ul> 
-     <Link to='/healthyfoods'>HealthyFoods </Link>|
-     <Link to='/healthydrinks'>healthy Drink   </Link>|
-     <Link to='/healthyfoods/new'> Add HealthyFoods</Link> |
-     <Link to='/healthydrinks/new'>new healthy Drink</Link>|
-     <Link to='/logout'>Logout </Link>|
-     </ul> 
+     <div  className="navbar-start">
+     <Link to='/healthyfoods' className="navbar-item">HealthyFoods </Link>
+     <Link to='/healthydrinks' className="navbar-item">healthy Drink   </Link>
+     <Link to='/healthyfoods/new' className="navbar-item"> Add HealthyFoods</Link> 
+     <Link to='/healthydrinks/new'className="navbar-item">new healthy Drink</Link>
+     <Link to='/logout'className="navbar-item">Logout </Link>
+     </div >
      </div>
      </nav>
-     
      </>
   )
 } 
